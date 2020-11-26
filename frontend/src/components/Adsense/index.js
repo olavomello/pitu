ce4bbacb1a3085b16c53292047a0c7b30bff05c6
Olavo Mello <olavo@agenciadix.com.br>
2020-11-26 20:55:25 -0300

    Added 'pub' information

--- frontend/src/components/Adsense/index.js
+++ frontend/src/components/Adsense/index.js
@@ -7,6 +7,7 @@ class Adsense  extends React.Component{
     render(){
         return(
             <>
+                <div><font size=1>Publicidade:</font></div>
                 <script data-ad-client="ca-pub-2533148814025391" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>            
             </>   
         );
