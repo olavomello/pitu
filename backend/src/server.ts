4f33fe4c2cda71b5e0f4e29e7f3577e7313f25ad
Olavo Mello <olavo@agenciadix.com.br>
2020-11-27 01:45:23 -0300

    Added SSL to API

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,5 +5,23 @@ import database from "./database"
 database.sync({force:false});
 console.log("DB  running...");
 
-app.listen(3001);
-console.log("Server running at 3001...");
+
+
+var fs = require('fs');
+//var http = require('http');
+var https = require('https');
+
+// SSL credentials
+var privateKey  = fs.readFileSync('sslcert/selfsigned.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/selfsigned.crt', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
+//var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+// app.listen(3001);
+// httpServer.listen(3000);
+httpsServer.listen(3001);
+
+
+console.log("Server running...");
